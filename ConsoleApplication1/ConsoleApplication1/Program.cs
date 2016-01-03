09c3df00b9bf3aa9a615530c90506c508320225b
Tristan Aurini <tjaurini@gmail.com>
2016-01-03 15:37:15 -0700

    put dlls and libs for opencv into the project (not sure if that was a good idea or not yet). created an object to represent the opencv section of the brain.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,5 +11,31 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
         }
+
+        private void maintwo(){
+        /*# include "opencv\highgui.h"
+            int main() {
+                cvNamedWindow("Test Window", CV_WINDOW_AUTOSIZE);
+                CvCapture* capture = cvCreateFileCapture("C:/Users/Tristan/Pictures/Projects/Rumchan/LiveImages/realRummyPics/101_0196.JPG");
+                IplImage* frame;
+                while (1)
+                {
+                    frame = cvQueryFrame(capture);
+                    if (!frame)
+                    {
+                        break;
+                    }
+                    cvShowImage("Test Window", frame);
+                    char c = cvWaitKey(0);
+                    if (c == 27)
+                    {   //hit 'esc'
+                        break;
+                    }
+                }
+                cvReleaseCapture(&capture);
+                cvDestroyWindow("Test Window");
+
+            }*/
+        }
     }
 }
